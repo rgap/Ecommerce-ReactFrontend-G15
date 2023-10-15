2405cd9bbe8eb9d138e3e6ab0075bc6f94da3303
jfalconidc <jorgefalconidc@gmail.com>
2023-10-14 19:33:59 -0500

    Cambio 1

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,30 +1,32 @@
-/* eslint-disable react/prop-types */
-export default function ProductCard({
-  productImage,
-  productTitle,
-  productText,
-  productPrice,
-  productColors,
-}) {
-  return (
-    <div>
-      <img
-        className="w-full object-contain"
-        src={productImage}
-        alt={productTitle}
-      />
-      <div>
-        <div className="font-bold leading-6">{productTitle}</div>
-        <div className="leading-6">{productText}</div>
+export default function ProductCard ({productImage,productTitle,productText,productPrice,productColors}){
+
+    return(
         <div>
-          <div className="font-semibold leading-6">{productPrice}</div>
+            <img
+              className="w-full object-contain"
+              src={productImage}
+              alt={productTitle}
+            />
+            <div>
+              <div className="font-bold leading-6">{productTitle}</div>
+              <div className="flex justify-between">
+              <div className="leading-6">{productText}</div>
+                <div className="font-bold text-right">{productPrice}</div>
+              </div>
+              <div className="flex justify-start gap-2 mt-2">
+                <div
+                  className="w-[24px] h-[24px] relative bg-blue-400 rounded-[24px]"
+                ></div>
+                <div
+                  className="w-[24px] h-[24px] relative bg-red-400 rounded-[24px]"
+                ></div>
+                <div
+                  className="w-[24px] h-[24px] relative bg-green-400 rounded-[24px]"
+                ></div>
+              </div>
+             
+            </div>
         </div>
-        <div className="inline-flex justify-start items-end gap-2">
-          <div className="w-[24px] h-[24px] relative bg-blue-400 rounded-[24px]"></div>
-          <div className="w-[24px] h-[24px] relative bg-red-400 rounded-[24px]"></div>
-          <div className="w-[24px] h-[24px] relative bg-green-400 rounded-[24px]"></div>
-        </div>
-      </div>
-    </div>
-  );
+    )
 }
+
