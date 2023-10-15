780470214ce985868fa689b9770cef2b4bb5a944
jfalconidc <jorgefalconidc@gmail.com>
2023-10-14 19:17:22 -0500

    Modificando el index.jsx de Product

---
+++ src/pages/Products/index.jsx
@@ -0,0 +1,257 @@
+export default function Products() {
+  return (
+    <>
+      <div class="py-8 pl-24 max-sm:pl-9 max-lg:pl-12">
+        <p>
+          <a>Inicio</a> / <a>Productos</a>
+        </p>
+      </div>
+      <div class="hero">
+        <div class="bord bg-[url('https://raw.githubusercontent.com/rgap/Ecommerce-G15-ImageRepository/main/images/banner-nobg.jpg')] h-[600px] bg-center bg-cover bg-no-repeat max-sm:h-[300px] max-lg:h-[400px]"></div>
+      </div>
+      <main class="mt-20 w-full max-lg:mt-10">
+        <section class="flex justify-around mb-20 max-lg:block max-lg:mb-10">
+          <div class="w-[357px] max-lg:m-auto max-lg:mt-10">
+            <img
+              src="https://raw.githubusercontent.com/rgap/Ecommerce-G15-ImageRepository/main/images/polo-verde-claro-nike-m-nobg.jpg"
+              alt=""
+              class="w-[357px] h-[350px] bg-center bg-contain bg-no-repeat"
+            />
+            <div class="mt-2">
+              <h3 class="font-bold">Polo Urban Green</h3>
+              <div class="flex justify-between">
+                <p>Algodon peinado 20 a 1</p>
+                <p class="font-bold text-right">S/78</p>
+              </div>
+              <div class="flex justify-start gap-2 mt-2">
+                <div class="w-5 h-5 bg-green-800 rounded-full"></div>
+                <div class="w-5 h-5 bg-green-300 rounded-full"></div>
+                <div class="w-5 h-5 bg-lime-500 rounded-full"></div>
+              </div>
+            </div>
+          </div>
+          <div class="w-[357px] max-lg:m-auto max-lg:mt-10">
+            <img
+              src="https://raw.githubusercontent.com/rgap/Ecommerce-G15-ImageRepository/main/images/polo-verde-claro-nike-m-nobg.jpg"
+              alt=""
+              class="w-[357px] h-[350px] bg-center bg-contain bg-no-repeat"
+            />
+            <div class="mt-2">
+              <h3 class="font-bold">Polo Style Green</h3>
+              <div class="flex justify-between">
+                <p>Algodon peinado 20 a 1</p>
+                <p class="font-bold text-right">S/78</p>
+              </div>
+              <div class="flex justify-start gap-2 mt-2">
+                <div class="w-5 h-5 bg-green-800 rounded-full"></div>
+                <div class="w-5 h-5 bg-green-300 rounded-full"></div>
+                <div class="w-5 h-5 bg-lime-500 rounded-full"></div>
+              </div>
+            </div>
+          </div>
+          <div class="w-[357px] max-lg:m-auto max-lg:mt-10">
+            <img
+              src="https://raw.githubusercontent.com/rgap/Ecommerce-G15-ImageRepository/main/images/polo-verde-claro-nike-m-nobg.jpg"
+              alt=""
+              class="w-[357px] h-[350px] bg-center bg-contain bg-no-repeat"
+            />
+            <div class="mt-2">
+              <h3 class="font-bold">Polo Sweet Green</h3>
+              <div class="flex justify-between">
+                <p>Algodon peinado 20 a 1</p>
+                <p class="font-bold text-right">S/78</p>
+              </div>
+              <div class="flex justify-start gap-2 mt-2">
+                <div class="w-5 h-5 bg-green-800 rounded-full"></div>
+                <div class="w-5 h-5 bg-green-300 rounded-full"></div>
+                <div class="w-5 h-5 bg-lime-500 rounded-full"></div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="flex justify-around mb-20 max-lg:block max-lg:mb-10">
+          <div class="w-[357px] max-lg:m-auto max-lg:mt-10">
+            <img
+              src="https://raw.githubusercontent.com/rgap/Ecommerce-G15-ImageRepository/main/images/polo-turqueza.png"
+              alt=""
+              class="w-[357px] h-[350px] bg-center bg-contain bg-no-repeat"
+            />
+            <div class="mt-2">
+              <h3 class="font-bold">Polo Urban Turquoise</h3>
+              <div class="flex justify-between">
+                <p>Algodon peinado 20 a 1</p>
+                <p class="font-bold text-right">S/80</p>
+              </div>
+              <div class="flex justify-start gap-2 mt-2">
+                <div class="w-5 h-5 bg-cyan-800 rounded-full"></div>
+                <div class="w-5 h-5 bg-cyan-200 rounded-full"></div>
+                <div class="w-5 h-5 bg-cyan-400 rounded-full"></div>
+              </div>
+            </div>
+          </div>
+          <div class="w-[357px] max-lg:m-auto max-lg:mt-10">
+            <img
+              src="https://raw.githubusercontent.com/rgap/Ecommerce-G15-ImageRepository/main/images/polo-turqueza.png"
+              alt=""
+              class="w-[357px] h-[350px] bg-center bg-contain bg-no-repeat"
+            />
+            <div class="mt-2">
+              <h3 class="font-bold">Polo Style Turquoise</h3>
+              <div class="flex justify-between">
+                <p>Algodon peinado 20 a 1</p>
+                <p class="font-bold text-right">S/80</p>
+              </div>
+              <div class="flex justify-start gap-2 mt-2">
+                <div class="w-5 h-5 bg-cyan-800 rounded-full"></div>
+                <div class="w-5 h-5 bg-cyan-200 rounded-full"></div>
+                <div class="w-5 h-5 bg-cyan-400 rounded-full"></div>
+              </div>
+            </div>
+          </div>
+          <div class="w-[357px] max-lg:m-auto max-lg:mt-10">
+            <img
+              src="https://raw.githubusercontent.com/rgap/Ecommerce-G15-ImageRepository/main/images/polo-turqueza.png"
+              alt=""
+              class="w-[357px] h-[350px] bg-center bg-contain bg-no-repeat"
+            />
+            <div class="mt-2">
+              <h3 class="font-bold">Polo Sweet Turquoise</h3>
+              <div class="flex justify-between">
+                <p>Algodon peinado 20 a 1</p>
+                <p class="font-bold text-right">S/80</p>
+              </div>
+              <div class="flex justify-start gap-2 mt-2">
+                <div class="w-5 h-5 bg-cyan-800 rounded-full"></div>
+                <div class="w-5 h-5 bg-cyan-200 rounded-full"></div>
+                <div class="w-5 h-5 bg-cyan-400 rounded-full"></div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="flex justify-around mb-20 max-lg:block max-lg:mb-10">
+          <div class="w-[357px] max-lg:m-auto max-lg:mt-10">
+            <img
+              src="https://raw.githubusercontent.com/rgap/Ecommerce-G15-ImageRepository/main/images/polo-%20amarillo-billabong-logo.jpg"
+              alt=""
+              class="w-[357px] h-[350px] bg-center bg-contain bg-no-repeat"
+            />
+            <div class="mt-2">
+              <h3 class="font-bold">Polo Urban Yellow</h3>
+              <div class="flex justify-between">
+                <p>Algodon peinado 20 a 1</p>
+                <p class="font-bold text-right">S/58</p>
+              </div>
+              <div class="flex justify-start gap-2 mt-2">
+                <div class="w-5 h-5 bg-yellow-500 rounded-full"></div>
+                <div class="w-5 h-5 bg-yellow-400 rounded-full"></div>
+                <div class="w-5 h-5 bg-yellow-200 rounded-full"></div>
+              </div>
+            </div>
+          </div>
+          <div class="w-[357px] max-lg:m-auto max-lg:mt-10">
+            <img
+              src="https://raw.githubusercontent.com/rgap/Ecommerce-G15-ImageRepository/main/images/polo-%20amarillo-billabong-logo.jpg"
+              alt=""
+              class="w-[357px] h-[350px] bg-center bg-contain bg-no-repeat"
+            />
+            <div class="mt-2">
+              <h3 class="font-bold">Polo Style Yellow</h3>
+              <div class="flex justify-between">
+                <p>Algodon peinado 20 a 1</p>
+                <p class="font-bold text-right">S/58</p>
+              </div>
+              <div class="flex justify-start gap-2 mt-2">
+                <div class="w-5 h-5 bg-yellow-500 rounded-full"></div>
+                <div class="w-5 h-5 bg-yellow-400 rounded-full"></div>
+                <div class="w-5 h-5 bg-yellow-200 rounded-full"></div>
+              </div>
+            </div>
+          </div>
+          <div class="w-[357px] max-lg:m-auto max-lg:mt-10">
+            <img
+              src="https://raw.githubusercontent.com/rgap/Ecommerce-G15-ImageRepository/main/images/polo-%20amarillo-billabong-logo.jpg"
+              alt=""
+              class="w-[357px] h-[350px] bg-center bg-contain bg-no-repeat"
+            />
+            <div class="mt-2">
+              <h3 class="font-bold">Polo Sweet Yellow</h3>
+              <div class="flex justify-between">
+                <p>Algodon peinado 20 a 1</p>
+                <p class="font-bold text-right">S/58</p>
+              </div>
+              <div class="flex justify-start gap-2 mt-2">
+                <div class="w-5 h-5 bg-yellow-500 rounded-full"></div>
+                <div class="w-5 h-5 bg-yellow-400 rounded-full"></div>
+                <div class="w-5 h-5 bg-yellow-200 rounded-full"></div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="flex justify-around mb-36 max-lg:block max-lg:mb-16">
+          <div class="w-[357px] max-lg:m-auto max-lg:mt-10">
+            <img
+              src="https://raw.githubusercontent.com/rgap/Ecommerce-G15-ImageRepository/main/images/polo-marron-ripcurl-floreado.jpg"
+              alt=""
+              class="w-[357px] h-[350px] bg-center bg-contain bg-no-repeat"
+            />
+            <div class="mt-2">
+              <h3 class="font-bold">Polo Urban Brown</h3>
+              <div class="flex justify-between">
+                <p>Algodon peinado 20 a 1</p>
+                <p class="font-bold text-right">S/75</p>
+              </div>
+              <div class="flex justify-start gap-2 mt-2">
+                <div class="w-5 h-5 bg-amber-900 rounded-full"></div>
+                <div class="w-5 h-5 bg-amber-800 rounded-full"></div>
+                <div class="w-5 h-5 bg-yellow-950 rounded-full"></div>
+              </div>
+            </div>
+          </div>
+          <div class="w-[357px] max-lg:m-auto max-lg:mt-10">
+            <img
+              src="https://raw.githubusercontent.com/rgap/Ecommerce-G15-ImageRepository/main/images/polo-marron-ripcurl-floreado.jpg"
+              alt=""
+              class="w-[357px] h-[350px] bg-center bg-contain bg-no-repeat"
+            />
+            <div class="mt-2">
+              <h3 class="font-bold">Polo Style Brown</h3>
+              <div class="flex justify-between">
+                <p>Algodon peinado 20 a 1</p>
+                <p class="font-bold text-right">S/75</p>
+              </div>
+              <div class="flex justify-start gap-2 mt-2">
+                <div class="w-5 h-5 bg-amber-900 rounded-full"></div>
+                <div class="w-5 h-5 bg-amber-800 rounded-full"></div>
+                <div class="w-5 h-5 bg-yellow-950 rounded-full"></div>
+              </div>
+            </div>
+          </div>
+          <div class="w-[357px] max-lg:m-auto max-lg:mt-10">
+            <img
+              src="https://raw.githubusercontent.com/rgap/Ecommerce-G15-ImageRepository/main/images/polo-marron-ripcurl-floreado.jpg"
+              alt=""
+              class="w-[357px] h-[350px] bg-center bg-contain bg-no-repeat"
+            />
+            <div class="mt-2">
+              <h3 class="font-bold">Polo Sweet Brown</h3>
+              <div class="flex justify-between">
+                <p>Algodon peinado 20 a 1</p>
+                <p class="font-bold text-right">S/75</p>
+              </div>
+              <div class="flex justify-start gap-2 mt-2">
+                <div class="w-5 h-5 bg-amber-900 rounded-full"></div>
+                <div class="w-5 h-5 bg-amber-800 rounded-full"></div>
+                <div class="w-5 h-5 bg-yellow-950 rounded-full"></div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <div class="mb-14">
+          <div class="w-40 h-10 border border-black shadow-md m-auto text-center p-1">
+            <button>Cargar Más</button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
