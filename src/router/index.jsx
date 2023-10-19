ecf2c673c23cb056a892fe31c8aebf76efc05072
jfalconidc <jorgefalconidc@gmail.com>
2023-10-19 03:32:28 -0500

    Modificacion en router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,11 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
-import { MainLayout } from "../layouts";
-import { Cart, Home, Products, ProductsDetails, Profile, Register } from "../pages";
-=======
 import { CartLayout, MainLayout } from "../layouts";
 import { Cart, Home, Login, Products, ProductsDetails, Profile, Register } from "../pages";
->>>>>>> main
 
 export default function AppRouter() {
   return (
