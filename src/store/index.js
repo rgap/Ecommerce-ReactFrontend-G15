5e714a061f849a06ded5e1f09edcbd7054424a28
anabelena <anabelen.aristah@gmail.com>
2023-10-25 15:06:33 -0700

    nada

--- src/store/index.js
+++ src/store/index.js
@@ -3,25 +3,8 @@ import counterSlice from "../slices/counterSlice";
 import userSlice from "../slices/userSlice";
 
 export const store = configureStore({
-<<<<<<< HEAD
-<<<<<<< HEAD
-    reducer: {
-      counter: counterSlice,
-    },
-  });
-  
-  
-=======
-=======
-
->>>>>>> 0e2220b73a7bcf8d901af003104b5dbdaf758456
   reducer: {
     counter: counterSlice,
     user: userSlice,
   },
 });
-<<<<<<< HEAD
->>>>>>> 824983792589fc39e8e817361c1ac6ee012083d9
-=======
-
->>>>>>> 0e2220b73a7bcf8d901af003104b5dbdaf758456
