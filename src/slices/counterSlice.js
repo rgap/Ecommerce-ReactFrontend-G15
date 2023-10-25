fc6b126d703be6d6187f9430849b4d5dba62ef6b
anabelena <anabelen.aristah@gmail.com>
2023-10-24 19:09:25 -0700

    utilizando redux en componente carrito

--- src/slices/counterSlice.js
+++ src/slices/counterSlice.js
@@ -13,44 +13,43 @@ export const counterSlice = createSlice({
     },
 
     increment: (state, action) => {
-      
       const productId = action.payload;
-      const productIndex = state.productos.findIndex(producto => producto.id === productId )
+      const productIndex = state.productos.findIndex(
+        (producto) => producto.id === productId
+      );
+
       if (productIndex >= 0) {
+        const newState2 = state.productos.map((item) => {
+          if (item.id === productId) {
+            return {
+              ...item,
+              cantidad: item.cantidad + 1,
+            };
+          }
+          return item;
+        });
 
-       const newState2 = state.productos.map(item => {
-         if (item.id === productId) {
-           return {
-             ...item,
-             cantidad: item.cantidad + 1
-           }
-         }
-         return item
-
-       })
-        debugger
-        return newState2
-
+        console.log({ newState2 });
+        return {
+          ...state,
+          productos: [...newState2],
+        };
       }
-     
-      
+
       const newState = [
         ...state.productos,
         {
           id: productId, // product
-          cantidad: 1
-        }
-      ]
+          cantidad: 1,
+        },
+      ];
 
-
-
-      const xyz =  {...state,productos:[...newState]}  
-      console.log(xyz)
-      return xyz
+      const xyz = { ...state, productos: [...newState] };
+      console.log(xyz);
+      return xyz;
     },
 
     decrement: (state, action) => {
-
       state.productos -= 1;
     },
   },
@@ -61,6 +60,5 @@ export const { initialize, increment, decrement } = counterSlice.actions;
 export const selectCounter = (state) => state.counter.value;
 
 export const selectProductos = (state) => state.counter.productos;
-
 
 export default counterSlice.reducer;
