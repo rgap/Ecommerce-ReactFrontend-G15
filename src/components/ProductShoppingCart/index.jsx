3964fd313527a2772d533ca3e902b26d1c759504
anabelena <anabelen.aristah@gmail.com>
2023-10-31 00:22:33 -0700

    avance componente cartinfo

--- src/components/ProductShoppingCart/index.jsx
+++ src/components/ProductShoppingCart/index.jsx
@@ -12,6 +12,7 @@ export default function ProductShoppingCart({
   productPrice,
   productQuantity,
   product,
+  visible,
 }) {
   const [isMobile, setIsMobile] = useState(false);
   const dispatch = useDispatch();
